4359b8fc683d23bbbb1875807b766554a26c458e
Md. Hossain <159703839+best-rion@users.noreply.github.com>
2025-04-24 01:56:55 +0600

    Update and rename ErrorController.java to ErrController.java

--- src/main/java/site/rion/PetShop/controller/ErrController.java
+++ src/main/java/site/rion/PetShop/controller/ErrController.java
@@ -1,10 +1,11 @@
 package site.rion.PetShop.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ErrorController
+public class ErrController implements ErrorController
 {
 	@GetMapping("/error")
 	public String handleError()
